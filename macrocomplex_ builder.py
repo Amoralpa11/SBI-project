97ed5b2739a9d70f6010f6cab3ff3d71abc51103
Marc Elosua <elosua.marc@gmail.com>
2018-03-13 15:59:30 +0100

    fixed clash detector

---
+++ macrocomplex_ builder.py
@@ -0,0 +1,7 @@
+def macrocomplex_builder(str_dict):
+    """
+    This function rebuilds a complex with the interactions we obtained from the pdb files.
+    :param str_dict: dictionary with all the interactions we want to build the complex with.
+    :return: returns a XXXXXXX with the macrocomplex built and... the middle steps??
+    """
+

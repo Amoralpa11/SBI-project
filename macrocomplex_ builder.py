0159d86c9dd0d37fa154accb014f93e7d45375a5
Amoralpa11 <drnmoralespastor@gmail.com>
2018-03-21 10:01:35 +0100

    Haciendo el log

--- macrocomplex_ builder.py
+++ macrocomplex_ builder.py
@@ -2,6 +2,7 @@ from Bio.PDB import *
 from Complex_breaker import *
 from Complex_id import *
 
+main_counter = 0
 
 def get_clash_chains(structure, chain):
     """
@@ -119,7 +120,14 @@ def superimpose_fun(str1, str2, node, i, complex_id, similar_seq):
 #########
 
 def update_structure(base_struct, complex_id, complex_id_dict, similar_seq, chains_str_dict):
-    print("Starting new Branch: ")
+
+    print("Starting new Branch:")
+
+    for node in complex_id.get_nodes():
+        print(node.get_chain())
+        for interaction, node in node.get_interaction_dict().get_item():
+            print("%s: %s " %(interaction, node))
+
     for other_CI in [ident for ident in complex_id_dict[len(complex_id.get_nodes())] if ident != complex_id]:
         if complex_id.compare_with(other_CI):
 
@@ -175,10 +183,10 @@ def macrocomplex_builder(id_dict, similar_seq, interaction_dict):
 
     # initialize a complex id dictionary
     complex_id_dict = {}
+
     global main_counter
-    main_counter = 0
     for chain in chains_str_dict:
-        print('Ininciando rama %n' % main_counter)
+        print('Ininciando rama %s' % main_counter)
         # initialize an empty structure
         base_struct = Structure.Structure('1')
         base_struct.add(Model.Model(0))

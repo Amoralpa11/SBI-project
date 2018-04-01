644d44825e5a388328b5b508dc5af301b2d6a185
Amoralpa11 <drnmoralespastor@gmail.com>
2018-04-01 17:57:07 +0200

    haciendo limpieza

---
+++ pmcb-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='pmcb',
+      version='1.0',
+      description='Protein Macro-Complex Builder',
+      author='Marc Elosua Bayés, Adrián Morales Pastor',
+      author_email='elosua.marc@gmail.com, drnmoralespastor@gmail.com',
+      py_modules=["Complex_breaker", "Complex_id", "macrocomplex_builder", 'modeller_optimization',
+                  'pdb_files_comparison'],
+      scripts=['build_compolex.py'])
--- setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-
-setup(name='pmcb',
-      version='1.0',
-      description='Protein Macro-Complex Builder',
-      author='Marc Elosua Bayés, Adrián Morales Pastor',
-      author_email='elosua.marc@gmail.com, drnmoralespastor@gmail.com',
-      py_modules=["Complex_breaker", "Complex_id", "macrocomplex_builder", 'modeller_optimization', 'pdb_files_comparison'])

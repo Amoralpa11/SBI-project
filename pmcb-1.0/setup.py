ea61edd8e0aa8c7d05d9c666ec1b4c1c87a4c9e8
Amoralpa11 <drnmoralespastor@gmail.com>
2018-04-03 16:07:13 +0200

    poniendo a punto la instalación

--- pmcb-1.0/setup.py
+++ pmcb-1.0/setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup, find_packages
-from codecs import open
 from os import path
 
 here = path.abspath(path.dirname(__file__))
@@ -12,6 +11,6 @@ setup(name='pmcb',
       url='https://github.com/Amoralpa11/SBI-project/tree/complex_breaker',
       author='Marc Elosua Bayés, Adrián Morales Pastor',
       author_email='elosua.marc@gmail.com, drnmoralespastor@gmail.com',
-      packages=find_packages(),
-      entry_points={'console_scripts': ['build_complex = build_complex:main']},
+      packages=['pmcb'],
+      entry_points={'console_scripts': ['build_complex = build_complex:main',],},
       install_requires=['biopython', 'matplotlib', 'pandas'])
